882ad8f802779dc633a8c5378c1e07ff63241659
endoffile78 <endoffile78@yahoo.com>
2021-03-30 20:01:32 -0500

    Add mpv

--- config.sh
+++ config.sh
@@ -25,7 +25,7 @@ REPO="http://alpha.us.repo.voidlinux.org"
 REPOS="void-repo-multilib void-repo-multilib-nonfree void-repo-nonfree"
 
 DE="xfce4"
-DE_EXTRAS="xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin libreoffice pinentry-gtk gnome-disk-utility greybird-themes connman-gtk firefox alacritty"
+DE_EXTRAS="xfce4-whiskermenu-plugin xfce4-pulseaudio-plugin libreoffice pinentry-gtk gnome-disk-utility greybird-themes connman-gtk firefox alacritty mpv"
 
 SHELL="zsh"
 
